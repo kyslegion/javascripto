739d16bdf0f95d7f24563ce04443814a12f10086
kyslegion <kyslegion@gmail.com>
2022-10-03 22:06:25 +0200

    aa

--- require/require.js
+++ require/require.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let path =require('path');
 
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+// let http = require('http').Server(app);
+// let io = require('socket.io')(http);
 
-module.exports={express,app,path,http,io}
+module.exports={express,app,path}
