59a0d882d5b0541a56cba8c8c46109a6c383f0ce
kyslegion <kyslegion@gmail.com>
2022-10-03 22:01:59 +0200

    hh

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let{express,app,path}=require('./require/require');
 // var express = require('express');
 // var app = express();
 // const path =require('path');
-// let http = require('http').Server(app);
 let http = require('http').Server(app);
 
 let port=4000;
