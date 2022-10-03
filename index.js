4bdef7e794ae35c9310da7c1e15ce0f21723e302
kyslegion <kyslegion@gmail.com>
2022-10-03 16:34:39 +0200

    v1

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+let port=4000;
+// let express = require('express');
+// let app = express();
+// let path =require('path');
+
+let{express,app,path,http,io}=require('./require/require');
+let{view}=require('./view/view');
+let{routes}=require('./route/route');
+let{catalogue}=require('./script/catalogue');
+let{socket_io}=require('./Socket.io/serveur/serveur');
+
+// let http = require('http').Server(app);
+let x =__dirname;
+require('dotenv').config()
+let dotenv=require('dotenv').config()
+const mysql = require('mysql2')
+const con= mysql.createConnection(process.env.DATABASE_URL)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
+
+routes(app,express,x,path);
+view(app,con);
+socket_io(io,catalogue,con);
+
+
+
+
+http.listen(port, '0.0.0.0',()=>{
+  console.log(port);
+});
