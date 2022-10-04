2edd0afecef9bafd6de36c6cb12f4986f1507a33
kyslegion <kyslegion@gmail.com>
2022-10-04 12:24:59 +0200

    k

--- index.js
+++ index.js
@@ -1,18 +1,97 @@
+let express = require('express');
+let app = express();
+// let path =require('path');
 
-let{express,app,http}=require('./require/require');
+let http = require('http').Server(app);
+// let io = require('socket.io')(http);
+// let{express,app,http}=require('./require/require');
+// var express = require('express');
+// var app = express();
 let port=4000;
 const path =require('path');
 http.listen(port,()=>{
 });
+// let http = require('http').Server(app);
+// require('dotenv').config()
+// const mysql = require('mysql2')
+// const con= mysql.createConnection(process.env.DATABASE_URL);
+
+// con.query(`SELECT * FROM livres `, function (err, result, fields) {
+//     let products=JSON.parse(JSON.stringify(result));
+//     console.log(result);
+// })
+
+
+// let dotenv=require('dotenv').config()
+// const mysql = require('mysql2')
+
+
+// app.listen(port,()=>{
+//   console.log(port);
+// });
+
+
+
+// let{view}=require('./view/view');
+// let{routes}=require('./route/route');
+// let{catalogue}=require('./script/catalogue');
+// let{socket_io}=require('./Socket.io/serveur/serveur');
+
+// routes(app,express,x,path);
+// view(app,con);
+// socket_io(io,catalogue,con);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname+ '/'));
 app.use(express.static(__dirname+ '/style/background/'));
+// app.use(express.static(__dirname+ '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
+// use res.render to load up an ejs view file
+//l
 
+// index page
+// app.get('/', function(req, res) {
+//   res.render('index.ejs');
+// });
+// app.get('/',(req,res)=>{
+// //   res.sendFile(path.join(__dirname, './connexion.html'));
+//   res.render('./index');
+//   // res.render('views/index.ejs');
+// });
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname, './connexion.html'));
+//     // res.render('views/index.ejs');
+// });
+app.get('/', function(req, res) {
+    res.render('index');
+});
+  
+// about page
+// app.get('/catalogue', function(req, res) {
+//   // res.render('views/catalogue.ejs');
+//   res.sendFile(path.join(__dirname, './zobi.html'));
+// });
+// app.get('/ejs', function(req, res) {
+//   res.render('index');
+//   // res.sendFile('index');
+// });
+// app.get('/',(req,res)=>{
+//     con.query(`SELECT * FROM livres `, function (err, result, fields) {
+//       // let products=JSON.parse(JSON.stringify(result));
+//       // console.log(result);
+//       res.render('index.ejs', {
+//       });
+  
+//     })
+    
+    
+// });
+// app.listen('3000',()=>{
+//     console.log("Serveur 3000");
+// })
